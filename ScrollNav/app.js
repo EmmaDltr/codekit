7d64d07a67732a33e03dfee80d4d3b7e0a03c6bf
Emma <emma.deltenre@student.heaj.be>
2023-11-16 16:04:05 +0100

    ScrollNav
    initial simple ScrollNav

---
+++ ScrollNav/app.js
@@ -0,0 +1,17 @@
+"use strict";
+let oldScrollY = 0;
+let offsetY = 250;
+let navigation = document.querySelector(".nav");
+window.addEventListener('scroll', function(e) {
+  if(oldScrollY > window.scrollY || window.scrollY < offsetY){
+    navigation.classList.remove("nav--hidden");
+  }else{
+    navigation.classList.add("nav--hidden");
+  }
+  oldScrollY = window.scrollY;
+});
+
+
+
+
+
