556270290675c86f483fe0471bd260db5fb35c67
Emma <emma.deltenre@student.heaj.be>
2023-11-23 14:44:28 +0100

    push

--- ScrollNav/app.js
+++ ScrollNav/app.js
@@ -1,7 +1,9 @@
 "use strict";
+
 let oldScrollY = 0;
 let offsetY = 250;
 let navigation = document.querySelector(".nav");
+
 window.addEventListener('scroll', function(e) {
   if(oldScrollY > window.scrollY || window.scrollY < offsetY){
     navigation.classList.remove("nav--hidden");
